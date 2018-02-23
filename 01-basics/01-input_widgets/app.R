c36ede924bbef23de9f9842a27c3f64be6fc809c
Johnson, Cal <Callen.Johnson@uscellular.com>
2018-02-23 14:04:12 -0600

    Modify app.R script to include a little more useful info for basics

--- 01-basics/01-input_widgets/app.R
+++ 01-basics/01-input_widgets/app.R
@@ -5,9 +5,9 @@ library(shiny)
 server <- function(input,output, session) {}
 
 ui <- basicPage(
-  h1("Using textInput and checkboxInput"),
-  textInput("mystring", "Write here"),
-  checkboxInput("mycheckbox", "Factor Y")
+  h1("Using textInput and checkboxInput"),  # header
+  textInput("mystring", "Write in box below", value = "text"),  # text box
+  checkboxInput("mycheckbox", "Factor Y")  # checkbox
 )
 
 shinyApp(ui = ui, server = server)
